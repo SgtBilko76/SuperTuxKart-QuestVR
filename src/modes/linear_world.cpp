SgtBilko76/SuperTuxKart-QuestVR

commit dd72ee6da278608730999fc16afb654a74fffe07

Added preliminary easter-egg mode. It still needs a icon (battle mode used atm), and a better easter egg model (atm bunny from snow mountain, ... whith turns out has lost the black nose). One track has easter eggs added (math class).
git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@12281 178a84e3-b1eb-0310-8ba1-8eac791a3b58

src/modes/linear_world.cpp
@@ -422,9 +422,7 @@ void LinearWorld::getKartsDisplayInfo(
         AbstractKart* kart = m_karts[i];
 
         // reset color
-        rank_info.r = 1.0;
-        rank_info.g = 1.0;
-        rank_info.b = 1.0;
+        rank_info.m_color = video::SColor(255, 255, 255, 255);
         rank_info.lap = -1;
 
         if(kart->isEliminated()) continue;
@@ -486,12 +484,15 @@ void LinearWorld::getKartsDisplayInfo(
 
         if(kart_info.m_race_lap>=numLaps)
         {  // kart is finished, display in green
-            rank_info.g = rank_info.b = 0;
+            rank_info.m_color.setGreen(0);
+            rank_info.m_color.setBlue(0);
         }
         else if(kart_info.m_race_lap>=0 && numLaps>1)
         {
-            rank_info.g = rank_info.b =
-                1.0f-(float)kart_info.m_race_lap/((float)numLaps-1.0f);
+            int col = (int)(255*(1.0f-(float)kart_info.m_race_lap
+                                    /((float)numLaps-1.0f)        ));
+            rank_info.m_color.setBlue(col);
+            rank_info.m_color.setGreen(col);
         }
     }   // next kart
 
@@ -561,22 +562,15 @@ void LinearWorld::moveKartAfterRescue(AbstractKart* kart)
 
     info.getSector()->rescue();
     int sector = info.getSector()->getCurrentGraphNode();
-    kart->setXYZ( QuadGraph::get()
-                  ->getQuadOfNode(sector).getCenter());
-
-    btQuaternion heading(btVector3(0.0f, 1.0f, 0.0f),
-                         m_track->getAngle(sector) );
-    kart->setRotation(heading);
 
     // A certain epsilon is added here to the Z coordinate, in case
     // that the drivelines are somewhat under the track. Otherwise, the
     // kart might be placed a little bit under the track, triggering
     // a rescue, ... (experimentally found value)
     float epsilon = 0.5f * kart->getKartHeight();
-
+    const Vec3 &xyz = QuadGraph::get()->getQuadOfNode(sector).getCenter();
     btTransform pos;
-    pos.setOrigin(kart->getXYZ()+btVector3(0, kart->getKartHeight() + epsilon,
-                                           0));
+    pos.setOrigin(xyz+btVector3(0, kart->getKartHeight() + epsilon,0));
     pos.setRotation(btQuaternion(btVector3(0.0f, 1.0f, 0.0f),
                     m_track->getAngle(sector)));
 

data/items.xml
@@ -9,5 +9,7 @@
        handled as one, so list it here    -->
   <bubblegum   model="bubblegum.b3d"       lowmodel="bubblegum-low.b3d"       />
   <bubblegum-nolok   model="bubblegum-nolok.b3d" lowmodel="bubblegum-nolok-low.b3d"/>
+  <!-- <easter-egg  model="easter_egg.b3d"  /> -->
+  <easter-egg  model="easter_egg.b3d"  />
 </items>
 

data/stk_config.xml
@@ -110,8 +110,9 @@
   <powerup collect-mode="new"/>
   <!-- time: How long a switch is being effective.
        items for each item list the index of the item it is switched with.
-             Order: giftbox, banana, big-nitro, small-nitro, bubble-gum, trigger, nolok-bubble-gum -->
-  <switch time="5"  items="1 0 4 4 2 5 2"/>
+             Order: giftbox, banana, big-nitro, small-nitro, bubble-gum, trigger,
+                    nolok-bubble-gum, easter egg -->
+  <switch time="5"  items="1 0 4 4 2 5 2 7"/>
 
   <!-- How often bubble gum get driven over before it disappears. --> 
   <bubble-gum disappear-counter="1"/>

sources.cmake
@@ -121,6 +121,7 @@ src/main.cpp
 src/main_loop.cpp
 src/modes/cutscene_world.cpp
 src/modes/demo_world.cpp
+src/modes/easter_egg_hunt.cpp
 src/modes/follow_the_leader.cpp
 src/modes/game_tutorial.cpp
 src/modes/linear_world.cpp
@@ -367,6 +368,7 @@ src/karts/skidding_properties.hpp
 src/main_loop.hpp
 src/modes/cutscene_world.hpp
 src/modes/demo_world.hpp
+src/modes/easter_egg_hunt.hpp
 src/modes/follow_the_leader.hpp
 src/modes/game_tutorial.hpp
 src/modes/linear_world.hpp

src/ide/vc9/supertuxkart.vcproj
@@ -701,6 +701,10 @@
 					>
 				</File>
 				<File
+					RelativePath="..\..\modes\easter_egg_hunt.cpp"
+					>
+				</File>
+				<File
 					RelativePath="..\..\modes\follow_the_leader.cpp"
 					>
 				</File>
@@ -1884,6 +1888,10 @@
 				</File>
 				<File
 					RelativePath="..\..\modes\demo_world.hpp"
+					>
+				</File>
+				<File
+					RelativePath="..\..\modes\easter_egg_hunt.hpp"
 					>
 				</File>
 				<File

commit 0cfd06be169cb233e6d9a43c4c65a16c3f962874

Fixed rescue (which I broke in r12281 - sorry).
git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@12325 178a84e3-b1eb-0310-8ba1-8eac791a3b58

src/modes/linear_world.cpp
@@ -561,8 +561,15 @@ void LinearWorld::moveKartAfterRescue(AbstractKart* kart)
     KartInfo& info = m_kart_info[kart->getWorldKartId()];
 
     info.getSector()->rescue();
+    // Setting XYZ for the kart is important since otherwise the kart
+    // will not detect the right material again when doing the next
+    // raycast to detect where it is driving on (--> potential rescue loop)
     int sector = info.getSector()->getCurrentGraphNode();
+    kart->setXYZ( QuadGraph::get()->getQuadOfNode(sector).getCenter());
 
+    btQuaternion heading(btVector3(0.0f, 1.0f, 0.0f), 
+                         m_track->getAngle(sector) );
+    kart->setRotation(heading);
     // A certain epsilon is added here to the Z coordinate, in case
     // that the drivelines are somewhat under the track. Otherwise, the
     // kart might be placed a little bit under the track, triggering