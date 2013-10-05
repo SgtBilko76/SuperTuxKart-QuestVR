SgtBilko76/SuperTuxKart-QuestVR

commit 86df2b12ef2786bce17963295b96bd695f9960d8

Added reverse-sorting of addons (#916). Thanks to ingridmorstrad for the patch.
git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@12673 178a84e3-b1eb-0310-8ba1-8eac791a3b58

src/guiengine/widgets/list_widget.cpp
@@ -39,6 +39,7 @@ ListWidget::ListWidget() : Widget(WTYPE_LIST)
     m_icons = NULL;
     m_listener = NULL;
     m_selected_column = NULL;
+    m_sort_desc = true;
 }
 
 // -----------------------------------------------------------------------------
@@ -371,6 +372,9 @@ EventPropagation ListWidget::transmitEvent(Widget* w,
         int col = originator[ (m_properties[PROP_ID] + "_column_").size() ] - '0';
 
         m_selected_column = m_header_elements.get(col);
+        
+        /** \brief Allows sort icon to change depending on sort order **/
+        m_sort_desc = !m_sort_desc;
         /*
         for (int n=0; n<m_header_elements.size(); n++)
         {

src/states_screens/addons_screen.cpp
@@ -93,6 +93,8 @@ void AddonsScreen::init()
     
     m_reloading = false;
     
+    m_sort_desc = true;
+    
 	getWidget<GUIEngine::RibbonWidget>("category")->setDeactivated();
 
     GUIEngine::getFont()->setTabStop(0.66f);
@@ -163,7 +165,7 @@ void AddonsScreen::loadList()
             continue;
         sorted_list.push_back(&addon);
     }
-    sorted_list.insertionSort(/*start=*/0);
+    sorted_list.insertionSort(/*start=*/0, m_sort_desc);
 
     GUIEngine::ListWidget* w_list = 
         getWidget<GUIEngine::ListWidget>("list_addons");
@@ -298,6 +300,8 @@ void AddonsScreen::onColumnClicked(int column_id)
     case 1: Addon::setSortOrder(Addon::SO_DATE); break;
     default: assert(0);
     }   // switch
+    /** \brief Toggle the sort order after column click **/
+    m_sort_desc = !m_sort_desc;
     loadList();
 }   // onColumnClicked
 

data/gui/skins/Glass.stkskin
@@ -175,6 +175,7 @@ when the border that intersect at this corner are enabled.
 <element type="list_header"  state="neutral" image="glass/table_header.png" />
 <element type="list_header"  state="down"    image="glass/table_header_down.png" />
 <element type="list_sort_up" state="neutral" image="glass/list_sort_up.png" />
+<element type="list_sort_down" state="neutral" image="glass/list_sort_down.png" />
 
 <!-- Stateless -->
 <element type="section" image="glass/glass_section.png"

data/gui/skins/Ocean.stkskin
@@ -174,6 +174,7 @@ when the border that intersect at this corner are enabled.
 
 <element type="list_header"  state="neutral" image="ocean/table_header.png" />
 <element type="list_sort_up" state="neutral" image="ocean/list_sort_up.png" />
+<element type="list_sort_down" state="neutral" image="ocean/list_sort_down.png" />
 <element type="list_header"  state="down"    image="ocean/table_header_down.png" />
 
 <!-- Stateless -->

data/gui/skins/Peach.stkskin
@@ -173,6 +173,7 @@ when the border that intersect at this corner are enabled.
 
 <element type="list_header"  state="neutral" image="peach/table_header.png" />
 <element type="list_sort_up" state="neutral" image="peach/list_sort_up.png" />
+<element type="list_sort_down" state="neutral" image="peach/list_sort_down.png" />
 <element type="list_header"  state="down"    image="peach/table_header_down.png" />
 
 <!-- Stateless -->

commit 046d18246e1a82b596387e06a34280af360faa5e

Fixed regression since r12673. Add-ons were sorting from "z" to "a" in default mode.
git-svn-id: svn+ssh://svn.code.sf.net/p/supertuxkart/code/main/trunk@14209 178a84e3-b1eb-0310-8ba1-8eac791a3b58

src/guiengine/widgets/list_widget.cpp
@@ -38,7 +38,7 @@ ListWidget::ListWidget() : Widget(WTYPE_LIST)
     m_icons = NULL;
     m_listener = NULL;
     m_selected_column = NULL;
-    m_sort_desc = true;
+    m_sort_desc = false;
     m_sort_default = true;
 }
 
@@ -300,7 +300,7 @@ void ListWidget::elementRemoved()
     }
     m_header_elements.clearAndDeleteAll();
     m_selected_column = NULL;
-    m_sort_desc = true;
+    m_sort_desc = false;
     m_sort_default = true;
 }
 
@@ -378,8 +378,8 @@ EventPropagation ListWidget::transmitEvent(Widget* w,
         }
         else
         {
-            m_sort_default = m_sort_desc && !m_sort_default;
             if (!m_sort_default) m_sort_desc = !m_sort_desc;
+            m_sort_default = !m_sort_desc && !m_sort_default;
         }
 
         m_sort_col = originator[(m_properties[PROP_ID] + "_column_").size()] - '0';

src/states_screens/addons_screen.cpp
@@ -136,7 +136,7 @@ void AddonsScreen::init()
 
     m_reloading = false;
 
-    m_sort_desc = true;
+    m_sort_desc = false;
     m_sort_default = true;
     m_sort_col = 0;
 
@@ -389,8 +389,8 @@ void AddonsScreen::onColumnClicked(int column_id)
     }
     else
     {
-        m_sort_default = m_sort_desc && !m_sort_default;
         if (!m_sort_default) m_sort_desc = !m_sort_desc;
+        m_sort_default = !m_sort_desc && !m_sort_default;
     }
     
     m_sort_col = column_id;