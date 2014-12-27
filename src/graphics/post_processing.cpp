parasgandhi/stk-code

Move all gfx related config to a separate file

src/graphics/post_processing.cpp
@@ -19,6 +19,7 @@
 
 #include "config/user_config.hpp"
 #include "graphics/callbacks.hpp"
+#include "central_settings.hpp"
 #include "graphics/camera.hpp"
 #include "graphics/glwrap.hpp"
 #include "graphics/irr_driver.hpp"
@@ -169,7 +170,7 @@ void PostProcessing::begin()
 /** Set the boost amount according to the speed of the camera */
 void PostProcessing::giveBoost(unsigned int camera_index)
 {
-    if (irr_driver->isGLSL())
+    if (CVS->isGLSL())
     {
         m_boost_time[camera_index] = 0.75f;
 
@@ -185,7 +186,7 @@ void PostProcessing::giveBoost(unsigned int camera_index)
  */
 void PostProcessing::update(float dt)
 {
-    if (!irr_driver->isGLSL())
+    if (!CVS->isGLSL())
         return;
 
     MotionBlurProvider* const cb =
@@ -307,7 +308,7 @@ void PostProcessing::renderGaussian6BlurLayer(FrameBuffer &in_fbo, size_t layer,
     GLuint LayerTex;
     glGenTextures(1, &LayerTex);
     glTextureView(LayerTex, GL_TEXTURE_2D, in_fbo.getRTT()[0], GL_R32F, 0, 1, layer, 1);
-    if (!irr_driver->hasARBComputeShaders())
+    if (!CVS->hasARBComputeShaders())
     {
         // Used as temp
         irr_driver->getFBO(FBO_SCALAR_1024).Bind();
@@ -367,7 +368,7 @@ void PostProcessing::renderGaussian6Blur(FrameBuffer &in_fbo, FrameBuffer &auxil
     assert(in_fbo.getWidth() == auxiliary.getWidth() && in_fbo.getHeight() == auxiliary.getHeight());
     float inv_width = 1.0f / in_fbo.getWidth(), inv_height = 1.0f / in_fbo.getHeight();
 
-    if (!irr_driver->hasARBComputeShaders())
+    if (!CVS->hasARBComputeShaders())
     {
         auxiliary.Bind();
 
@@ -447,11 +448,11 @@ void PostProcessing::renderHorizontalBlur(FrameBuffer &in_fbo, FrameBuffer &auxi
 void PostProcessing::renderGaussian17TapBlur(FrameBuffer &in_fbo, FrameBuffer &auxiliary)
 {
     assert(in_fbo.getWidth() == auxiliary.getWidth() && in_fbo.getHeight() == auxiliary.getHeight());
-    if (irr_driver->hasARBComputeShaders())
+    if (CVS->hasARBComputeShaders())
         glMemoryBarrier(GL_FRAMEBUFFER_BARRIER_BIT);
     float inv_width = 1.0f / in_fbo.getWidth(), inv_height = 1.0f / in_fbo.getHeight();
     {
-        if (!irr_driver->hasARBComputeShaders())
+        if (!CVS->hasARBComputeShaders())
         {
             auxiliary.Bind();
             FullScreenShader::Gaussian17TapHShader::getInstance()->SetTextureUnits(in_fbo.getRTT()[0], irr_driver->getFBO(FBO_LINEAR_DEPTH).getRTT()[0]);
@@ -467,10 +468,10 @@ void PostProcessing::renderGaussian17TapBlur(FrameBuffer &in_fbo, FrameBuffer &a
             glDispatchCompute((int)in_fbo.getWidth() / 8 + 1, (int)in_fbo.getHeight() / 8 + 1, 1);
         }
     }
-    if (irr_driver->hasARBComputeShaders())
+    if (CVS->hasARBComputeShaders())
         glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
     {
-        if (!irr_driver->hasARBComputeShaders())
+        if (!CVS->hasARBComputeShaders())
         {
             in_fbo.Bind();
 
@@ -487,7 +488,7 @@ void PostProcessing::renderGaussian17TapBlur(FrameBuffer &in_fbo, FrameBuffer &a
             glDispatchCompute((int)in_fbo.getWidth() / 8 + 1, (int)in_fbo.getHeight() / 8 + 1, 1);
         }
     }
-    if (irr_driver->hasARBComputeShaders())
+    if (CVS->hasARBComputeShaders())
         glMemoryBarrier(GL_TEXTURE_FETCH_BARRIER_BIT);
 }
 
@@ -843,7 +844,7 @@ FrameBuffer *PostProcessing::render(scene::ICameraSceneNode * const camnode, boo
     }
 
     // Workaround a bug with srgb fbo on sandy bridge windows
-    if (irr_driver->needUBOWorkaround())
+    if (CVS->needUBOWorkaround())
         return in_fbo;
 
     glEnable(GL_FRAMEBUFFER_SRGB);

src/graphics/render_skybox.cpp
@@ -1,3 +1,4 @@
+#include "central_settings.hpp"
 #include "graphics/IBL.hpp"
 #include "graphics/irr_driver.hpp"
 #include "graphics/shaders.hpp"
@@ -263,7 +264,7 @@ GLuint generateCubeMapFromTextures(const std::vector<video::ITexture *> &texture
         }
 
         glBindTexture(GL_TEXTURE_CUBE_MAP, result);
-        if (irr_driver->usesTextureCompression())
+        if (CVS->usesTextureCompression())
             glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_COMPRESSED_SRGB_ALPHA, size, size, 0, GL_BGRA, GL_UNSIGNED_BYTE, (GLvoid*)rgba[i]);
         else
             glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_SRGB_ALPHA, size, size, 0, GL_BGRA, GL_UNSIGNED_BYTE, (GLvoid*)rgba[i]);

src/graphics/stkscenemanager.hpp
@@ -5,6 +5,7 @@
 #define HEADER_STKSCENEMANAGER_HPP
 
 #include "utils/singleton.hpp"
+#include "central_settings.hpp"
 #include "gl_headers.hpp"
 #include "stkmesh.hpp"
 #include "gpuparticles.hpp"
@@ -20,7 +21,7 @@ public:
     {
         glGenBuffers(1, &drawindirectcmd);
         glBindBuffer(GL_DRAW_INDIRECT_BUFFER, drawindirectcmd);
-        if (irr_driver->hasBufferStorageExtension())
+        if (CVS->hasBufferStorageExtension())
         {
             glBufferStorage(GL_DRAW_INDIRECT_BUFFER, 10000 * sizeof(DrawElementsIndirectCommand), 0, GL_MAP_PERSISTENT_BIT | GL_MAP_WRITE_BIT);
             Ptr = (DrawElementsIndirectCommand *)glMapBufferRange(GL_DRAW_INDIRECT_BUFFER, 0, 10000 * sizeof(DrawElementsIndirectCommand), GL_MAP_PERSISTENT_BIT | GL_MAP_WRITE_BIT);

src/graphics/texturemanager.cpp
@@ -1,3 +1,4 @@
+#include "central_settings.hpp"
 #include "texturemanager.hpp"
 #include <fstream>
 #include <sstream>
@@ -34,7 +35,7 @@ void compressTexture(irr::video::ITexture *tex, bool srgb, bool premul_alpha)
     glBindTexture(GL_TEXTURE_2D, getTextureGLuint(tex));
 
     std::string cached_file;
-    if (irr_driver->usesTextureCompression())
+    if (CVS->usesTextureCompression())
     {
         // Try to retrieve the compressed texture in cache
         std::string tex_name = irr_driver->getTextureName(tex);
@@ -70,7 +71,7 @@ void compressTexture(irr::video::ITexture *tex, bool srgb, bool premul_alpha)
         }
     }
 
-    if (!irr_driver->usesTextureCompression())
+    if (!CVS->usesTextureCompression())
     {
         if (srgb)
             internalFormat = (tex->hasAlpha()) ? GL_SRGB_ALPHA : GL_SRGB;
@@ -88,7 +89,7 @@ void compressTexture(irr::video::ITexture *tex, bool srgb, bool premul_alpha)
     glGenerateMipmap(GL_TEXTURE_2D);
     delete[] data;
 
-    if (irr_driver->usesTextureCompression() && !cached_file.empty())
+    if (CVS->usesTextureCompression() && !cached_file.empty())
     {
         // Save the compressed texture in the cache for later use.
         saveCompressedTexture(cached_file);

SgtBilko76/SuperTuxKart-QuestVR

src/graphics/post_processing.cpp
@@ -19,6 +19,7 @@
 
 #include "config/user_config.hpp"
 #include "graphics/callbacks.hpp"
+#include "central_settings.hpp"
 #include "graphics/camera.hpp"
 #include "graphics/glwrap.hpp"
 #include "graphics/irr_driver.hpp"
@@ -169,7 +170,7 @@ void PostProcessing::begin()
 /** Set the boost amount according to the speed of the camera */
 void PostProcessing::giveBoost(unsigned int camera_index)
 {
-    if (irr_driver->isGLSL())
+    if (CVS->isGLSL())
     {
         m_boost_time[camera_index] = 0.75f;
 
@@ -185,7 +186,7 @@ void PostProcessing::giveBoost(unsigned int camera_index)
  */
 void PostProcessing::update(float dt)
 {
-    if (!irr_driver->isGLSL())
+    if (!CVS->isGLSL())
         return;
 
     MotionBlurProvider* const cb =
@@ -307,7 +308,7 @@ void PostProcessing::renderGaussian6BlurLayer(FrameBuffer &in_fbo, size_t layer,
     GLuint LayerTex;
     glGenTextures(1, &LayerTex);
     glTextureView(LayerTex, GL_TEXTURE_2D, in_fbo.getRTT()[0], GL_R32F, 0, 1, layer, 1);
-    if (!irr_driver->hasARBComputeShaders())
+    if (!CVS->supportsComputeShadersFiltering())
     {
         // Used as temp
         irr_driver->getFBO(FBO_SCALAR_1024).Bind();
@@ -367,7 +368,7 @@ void PostProcessing::renderGaussian6Blur(FrameBuffer &in_fbo, FrameBuffer &auxil
     assert(in_fbo.getWidth() == auxiliary.getWidth() && in_fbo.getHeight() == auxiliary.getHeight());
     float inv_width = 1.0f / in_fbo.getWidth(), inv_height = 1.0f / in_fbo.getHeight();
 
-    if (!irr_driver->hasARBComputeShaders())
+    if (!CVS->supportsComputeShadersFiltering())
     {
         auxiliary.Bind();
 
@@ -447,11 +448,11 @@ void PostProcessing::renderHorizontalBlur(FrameBuffer &in_fbo, FrameBuffer &auxi
 void PostProcessing::renderGaussian17TapBlur(FrameBuffer &in_fbo, FrameBuffer &auxiliary)
 {
     assert(in_fbo.getWidth() == auxiliary.getWidth() && in_fbo.getHeight() == auxiliary.getHeight());
-    if (irr_driver->hasARBComputeShaders())
+    if (CVS->supportsComputeShadersFiltering())
         glMemoryBarrier(GL_FRAMEBUFFER_BARRIER_BIT);
     float inv_width = 1.0f / in_fbo.getWidth(), inv_height = 1.0f / in_fbo.getHeight();
     {
-        if (!irr_driver->hasARBComputeShaders())
+        if (!CVS->supportsComputeShadersFiltering())
         {
             auxiliary.Bind();
             FullScreenShader::Gaussian17TapHShader::getInstance()->SetTextureUnits(in_fbo.getRTT()[0], irr_driver->getFBO(FBO_LINEAR_DEPTH).getRTT()[0]);
@@ -467,10 +468,10 @@ void PostProcessing::renderGaussian17TapBlur(FrameBuffer &in_fbo, FrameBuffer &a
             glDispatchCompute((int)in_fbo.getWidth() / 8 + 1, (int)in_fbo.getHeight() / 8 + 1, 1);
         }
     }
-    if (irr_driver->hasARBComputeShaders())
+    if (CVS->supportsComputeShadersFiltering())
         glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
     {
-        if (!irr_driver->hasARBComputeShaders())
+        if (!CVS->supportsComputeShadersFiltering())
         {
             in_fbo.Bind();
 
@@ -487,7 +488,7 @@ void PostProcessing::renderGaussian17TapBlur(FrameBuffer &in_fbo, FrameBuffer &a
             glDispatchCompute((int)in_fbo.getWidth() / 8 + 1, (int)in_fbo.getHeight() / 8 + 1, 1);
         }
     }
-    if (irr_driver->hasARBComputeShaders())
+    if (CVS->supportsComputeShadersFiltering())
         glMemoryBarrier(GL_TEXTURE_FETCH_BARRIER_BIT);
 }
 
@@ -843,7 +844,7 @@ FrameBuffer *PostProcessing::render(scene::ICameraSceneNode * const camnode, boo
     }
 
     // Workaround a bug with srgb fbo on sandy bridge windows
-    if (irr_driver->needUBOWorkaround())
+    if (!CVS->isARBUniformBufferObjectUsable())
         return in_fbo;
 
     glEnable(GL_FRAMEBUFFER_SRGB);

src/graphics/render_skybox.cpp
@@ -1,3 +1,4 @@
+#include "central_settings.hpp"
 #include "graphics/IBL.hpp"
 #include "graphics/irr_driver.hpp"
 #include "graphics/shaders.hpp"
@@ -263,7 +264,7 @@ GLuint generateCubeMapFromTextures(const std::vector<video::ITexture *> &texture
         }
 
         glBindTexture(GL_TEXTURE_CUBE_MAP, result);
-        if (irr_driver->usesTextureCompression())
+        if (CVS->isTextureCompressionEnabled())
             glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_COMPRESSED_SRGB_ALPHA, size, size, 0, GL_BGRA, GL_UNSIGNED_BYTE, (GLvoid*)rgba[i]);
         else
             glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_SRGB_ALPHA, size, size, 0, GL_BGRA, GL_UNSIGNED_BYTE, (GLvoid*)rgba[i]);

src/graphics/stkscenemanager.hpp
@@ -5,6 +5,7 @@
 #define HEADER_STKSCENEMANAGER_HPP
 
 #include "utils/singleton.hpp"
+#include "central_settings.hpp"
 #include "gl_headers.hpp"
 #include "stkmesh.hpp"
 #include "gpuparticles.hpp"
@@ -20,7 +21,7 @@ public:
     {
         glGenBuffers(1, &drawindirectcmd);
         glBindBuffer(GL_DRAW_INDIRECT_BUFFER, drawindirectcmd);
-        if (irr_driver->hasBufferStorageExtension())
+        if (CVS->isARBBufferStorageUsable())
         {
             glBufferStorage(GL_DRAW_INDIRECT_BUFFER, 10000 * sizeof(DrawElementsIndirectCommand), 0, GL_MAP_PERSISTENT_BIT | GL_MAP_WRITE_BIT);
             Ptr = (DrawElementsIndirectCommand *)glMapBufferRange(GL_DRAW_INDIRECT_BUFFER, 0, 10000 * sizeof(DrawElementsIndirectCommand), GL_MAP_PERSISTENT_BIT | GL_MAP_WRITE_BIT);

src/graphics/texturemanager.cpp
@@ -1,3 +1,4 @@
+#include "central_settings.hpp"
 #include "texturemanager.hpp"
 #include <fstream>
 #include <sstream>
@@ -34,7 +35,7 @@ void compressTexture(irr::video::ITexture *tex, bool srgb, bool premul_alpha)
     glBindTexture(GL_TEXTURE_2D, getTextureGLuint(tex));
 
     std::string cached_file;
-    if (irr_driver->usesTextureCompression())
+    if (CVS->isTextureCompressionEnabled())
     {
         // Try to retrieve the compressed texture in cache
         std::string tex_name = irr_driver->getTextureName(tex);
@@ -70,7 +71,7 @@ void compressTexture(irr::video::ITexture *tex, bool srgb, bool premul_alpha)
         }
     }
 
-    if (!irr_driver->usesTextureCompression())
+    if (!CVS->isTextureCompressionEnabled())
     {
         if (srgb)
             internalFormat = (tex->hasAlpha()) ? GL_SRGB_ALPHA : GL_SRGB;
@@ -88,7 +89,7 @@ void compressTexture(irr::video::ITexture *tex, bool srgb, bool premul_alpha)
     glGenerateMipmap(GL_TEXTURE_2D);
     delete[] data;
 
-    if (irr_driver->usesTextureCompression() && !cached_file.empty())
+    if (CVS->isTextureCompressionEnabled() && !cached_file.empty())
     {
         // Save the compressed texture in the cache for later use.
         saveCompressedTexture(cached_file);