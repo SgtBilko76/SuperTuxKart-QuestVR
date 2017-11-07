SgtBilko76/SuperTuxKart-QuestVR

commit 24637c21a5962194978219699ae0616155280cbe

OGL32CTX: Always return true when a feature is queried

lib/irrlicht/source/Irrlicht/COpenGLExtensionHandler.cpp
@@ -94,7 +94,7 @@ COpenGLExtensionHandler::COpenGLExtensionHandler() :
 #endif // _IRR_OPENGL_USE_EXTPOINTER_
 {
 	for (u32 i=0; i<IRR_OpenGL_Feature_Count; ++i)
-		FeatureAvailable[i]=false;
+		FeatureAvailable[i]=true;
 	DimAliasedLine[0]=1.f;
 	DimAliasedLine[1]=1.f;
 	DimAliasedPoint[0]=1.f;

lib/irrlicht/source/Irrlicht/COpenGLDriver.h
@@ -142,8 +142,6 @@ namespace video
 		//! queries the features of the driver, returns true if feature is available
 		virtual bool queryFeature(E_VIDEO_DRIVER_FEATURE feature) const
 		{
-		  if (feature == EVDF_FRAMEBUFFER_OBJECT)
-			 return true;
 			return FeatureEnabled[feature] && COpenGLExtensionHandler::queryFeature(feature);
 		}
 

commit 3412ebb50047564f4f5b34478660f0684c013ee7

Initialize FeatureAvailable array to false for legacy pipeline.
It has been broken in commit 24637c21a5962194978219699ae0616155280cbe.

It solves some issues with etnaviv gallium driver. It may also solve a crash on old intel 965gm on windows, but I can't test it atm.

lib/irrlicht/source/Irrlicht/COpenGLExtensionHandler.cpp
@@ -336,15 +336,16 @@ void COpenGLExtensionHandler::initExtensions(bool stencilBuffer, bool useCoreCon
 	else
 		os::Printer::log("OpenGL driver version is not 1.2 or better.", ELL_WARNING);
 
+	if (!useCoreContext)
 	{
+		for (u32 i = 0; i < IRR_OpenGL_Feature_Count; i++)
+			FeatureAvailable[i] = false;
+			
 		const char* t = NULL;
 		size_t len = 0;
 		c8 *str = 0;
 		
-		if (!useCoreContext)
-		{
-			t = reinterpret_cast<const char*>(glGetString(GL_EXTENSIONS));
-		}
+		t = reinterpret_cast<const char*>(glGetString(GL_EXTENSIONS));
 		
 		if (t)
 		{

lib/irrlicht/source/Irrlicht/COpenGLExtensionHandler.h
@@ -932,8 +932,6 @@ class COpenGLExtensionHandler
 	//! queries the features of the driver, returns true if feature is available
 	bool queryOpenGLFeature(EOpenGLFeatures feature) const
 	{
-	  if (COpenGLExtensionHandler::IRR_EXT_packed_depth_stencil)
-		 return true;
 		return FeatureAvailable[feature];
 	}
 