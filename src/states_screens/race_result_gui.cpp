SgtBilko76/SuperTuxKart-QuestVR

commit 3399a0136bdb2b1066070e0a55314ee509133855

Fix #5419
- Increase the character limit for the name before forced scrolling from 15 to 30. Beyond 30, many parts of the GUI become problematic anyways, and while many reasonable usernames exceed 15, none exceed 30.
- Use the actual rendered width of the player name to compute how many characters can be shown at once if scrolling is needed.
-- Also check the width of substrings to avoid overflows from uneven character lengths
- Add a short pause at the beginning and the end of the name scrolling, as otherwise there is little time to see the first and last characters compared to others

src/states_screens/race_result_gui.cpp
@@ -1931,21 +1931,50 @@ int RaceResultGUI::displayHighscores(int x, int y, bool increase_density)
     std::string kart_name;
     irr::core::stringw player_name;
 
-    // prevent excessive long name
-    unsigned int max_characters = 15;
-    unsigned int max_width = (UserConfigParams::m_width / 2 - 200) / 10;
-    if (max_width < 15)
-        max_characters = max_width;
-
+    const float SCORE_X_RATIO = 0.85f;
     int width_icon_adjusted = increase_density ? m_width_icon * 0.8f : m_width_icon;
+
+    // Used to prevent excessively long names
+    unsigned int max_width = (int)((float)UserConfigParams::m_width * (SCORE_X_RATIO - 0.65f))
+                             - width_icon_adjusted - 20;
 
     float time;
     for (int i = 0; i < scores->getNumberEntries(); i++)
     {
         scores->getEntry(i, kart_name, player_name, &time);
-        if (player_name.size() > max_characters)
+        float width_ratio = (float)GUIEngine::getSmallFont()->getDimension(player_name.c_str()).Width / (float)max_width;
+        unsigned int max_characters = 30;
+        if (player_name.size() > max_characters || width_ratio >= 1.0f)
         {
-            int begin = (int(m_timer / 0.4f)) % (player_name.size() - max_characters + 1);
+            // Different parts of a name can have different width properties:
+            // - We could assume all the characters are large, but that can waste a lot of space
+            // - Instead, we do some extra computations to use more space while avoiding overflows
+            find_length_start:
+            int substring_test_length = (int)((float)player_name.size() / width_ratio);
+            unsigned int max_substring_width = 0;
+            for (uint i=0; i<(player_name.size() - substring_test_length + 1); i++)
+            {
+                irr::core::stringw test_substring = player_name.subString(i, substring_test_length, false);
+                max_substring_width = std::max(max_substring_width,
+                                        GUIEngine::getSmallFont()->getDimension(test_substring.c_str()).Width);
+            }
+            float substring_overflow_factor = ((float)max_substring_width / (float)max_width);
+            // If the initial estimate was really off, we refine our estimate
+            if (substring_overflow_factor > 1.1f)
+            {
+                width_ratio *= substring_overflow_factor;
+                goto find_length_start;
+            }
+
+            float overflow_factor = substring_overflow_factor * width_ratio * 1.01f;
+            max_characters = (unsigned int)((float)player_name.size() / overflow_factor);
+            int overflow_chars = player_name.size() - max_characters;
+
+            // Add 1 to the divisor to ensure the last character is displayed, and add 2 for pauses
+            int begin = (int(m_timer / 0.4f)) % (overflow_chars + 3) - 1;
+            if (begin == -1) begin = 0; // Pause at the start
+            if (begin == overflow_chars + 1) begin = overflow_chars; // Pause at the end
+
             player_name = player_name.subString(begin, max_characters, false);
         }
 
@@ -1986,7 +2015,7 @@ int RaceResultGUI::displayHighscores(int x, int y, bool increase_density)
             core::recti(current_x, current_y, current_x + 150, current_y + 10),
                 text_color, false, false, NULL, true /* ignoreRTL */);
 
-        current_x = (int)(UserConfigParams::m_width * 0.85f);
+        current_x = (int)(UserConfigParams::m_width * SCORE_X_RATIO);
 
         // Finally draw the time
         std::string highscore_string;

commit 7fbb7600d4373941b8a427762fb1a5c0c1a9462a

Adjust the Tracks and GPs screen to better fit the random track button
Also fix the portability issue from uint introduced in 3399a01

data/gui/screens/tracks_and_gp.stkgui
@@ -3,38 +3,61 @@
     <icon-button id="back" x="1%" y="0" height="9%" icon="gui/icons/back.png"/>
 
     <div x="1%" y="0" width="98%" height="98%" layout="vertical-row" >
-        <header height="9%" I18N="In the track and grand prix selection screen" text="Grand Prix"
-                align="center" text_align="center" />
+        <div width="100%" proportion="5" layout="horizontal-row">
+            <div width="82%" height="100%" layout="vertical-row">
+                <header height="21%" I18N="In the track and grand prix selection screen" text="Grand Prix"
+                        align="center" text_align="center" />
 
-        <div width="100%" proportion="3" layout="horizontal-row" >
-            <box width="80%" height="100%" layout="vertical-row" padding="0">
-                <scrollable_toolbar id="gps" x="1%" y="1%" width="98%" height="98%" align="center" label_location="each"
-                                    square_items="true" child_width="165" child_height="120" />
-            </box>
+                <spacer width="100%" height="1%" />
+
+                <box width="100%" height="61%" layout="vertical-row" padding="0">
+                    <scrollable_toolbar id="gps" x="1%" y="1%" width="98%" height="98%" align="center" label_location="each"
+                                        square_items="true" child_width="165" child_height="120" />
+                </box>
+
+                <spacer width="100%" height="1%" />
+
+                <div width="100%" height="16%" layout="horizontal-row" >
+                    <header width="35%" height="100%" I18N="In the track and grand prix selection screen" text="All Tracks"
+                            align="center" text_align="center" />
+                    <textbox width="30%" align="center" id="search"/>
+
+                    <spacer width="3%"/>
+
+                    <div width="fit" height="100%" layout="horizontal-row" align="center">
+                        <checkbox id="favorite" align="center"/>
+                        <spacer width="5%"/>
+                        <tiny-header width="fit" height="100%" I18N="In the track and grand prix selection screen" text="Edit favorite tracks"/>
+                    </div>
+                </div>
+            </div>
+
             <spacer width="2%" height="20" />
-            <box width="18%" height="100%" layout="vertical-row">
-                <icon-button id="rand-gp" width="100%" proportion="1" icon="gui/icons/gp_new.png" align="center"
-                        I18N="In the track and grand prix selection screen" text="Random Grand Prix"/>
-                <spacer width="20" height="1f" />
-            </box>
-        </div>
 
-        <spacer width="100%" height="1%" />
-
-        <div width="100%" height="fit" layout="horizontal-row" >
-            <header width="30%" I18N="In the track and grand prix selection screen" text="All Tracks"
-                    align="center" text_align="center" />
-            <textbox width="30%" id="search"/>
-            <icon-button id="random_track" width="10%" height="100%" icon="gui/icons/track_random.png" I18N="In the track and grand prix selection screen" text=""/>
-            <div width="fit" height="100%" layout="horizontal-row" align="center">
-                <checkbox id="favorite"/>
-                <tiny-header width="fit" height="100%" I18N="In the track and grand prix selection screen" text="Edit favorite tracks"/>
+            <div width="16%" height="100%" layout="vertical-row">
+                <spacer width="20" height="2%" />
+                <box width="100%" height="48%" layout="vertical-row" padding="0">
+                    <!-- The icon-button doesn't properly reserve space for the label, so we use a separate label -->
+                    <icon-button id="rand-gp" width="100%" height="70%" icon="gui/icons/gp_new.png" align="center">
+                    <spacer width="20" height="1%" />
+                    <label width="100%" height="25%" I18N="In the track and grand prix selection screen"
+                                text="Random Grand Prix" word_wrap="true" align="center" text_align="center"/>
+                    <spacer width="20" height="4%" />
+                </box>
+                <spacer width="20" height="2%" />
+                <box width="100%" height="48%" layout="vertical-row" padding="0">
+                    <icon-button id="random_track" width="100%" height="70%" icon="gui/icons/track_random.png"/>
+                    <spacer width="20" height="1%" />
+                    <label width="100%" height="25%" I18N="In the track and grand prix selection screen"
+                                text="Random Track" word_wrap="true" align="center" text_align="center"/>
+                    <spacer width="20" height="4%" />
+                </box>
             </div>
         </div>
 
         <spacer width="100%" height="1%" />
 
-        <box proportion="8" width="100%" layout="vertical-row" padding="1">
+        <box width="100%" proportion="8" layout="vertical-row" padding="1">
             <ribbon_grid id="tracks" proportion="1" width="100%" square_items="true"
                 label_location="bottom" align="center"
                 child_width="176" child_height="132" />

src/states_screens/race_result_gui.cpp
@@ -1952,7 +1952,7 @@ int RaceResultGUI::displayHighscores(int x, int y, bool increase_density)
             find_length_start:
             int substring_test_length = (int)((float)player_name.size() / width_ratio);
             unsigned int max_substring_width = 0;
-            for (uint i=0; i<(player_name.size() - substring_test_length + 1); i++)
+            for (unsigned int i=0; i<(player_name.size() - substring_test_length + 1); i++)
             {
                 irr::core::stringw test_substring = player_name.subString(i, substring_test_length, false);
                 max_substring_width = std::max(max_substring_width,