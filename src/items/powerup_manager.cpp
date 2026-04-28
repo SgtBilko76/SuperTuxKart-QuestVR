SgtBilko76/SuperTuxKart-QuestVR

commit 638b92fe757d99aaed6260402d88219dfe59805d

Minor code cleanup
- Remove a variable marked as obsolete that didn't serve any purpose
- Make the logic for how many powerups are obtained cleaner and shorter

src/items/powerup_manager.cpp
@@ -893,23 +893,12 @@ PowerupManager::PowerupType PowerupManager::getRandomPowerup(unsigned int pos,
                                                              uint64_t random_number)
 {
     int powerup = m_current_item_weights.getRandomItem(pos-1, random_number);
-    if(powerup > POWERUP_LAST)
-    {
-        powerup -= (POWERUP_LAST-POWERUP_FIRST+1);
-        if(powerup > POWERUP_LAST)
-        {
-            powerup -= (POWERUP_LAST-POWERUP_FIRST+1);
-            *n = 3;
-        }
-        else
-        {
-            *n = 2;
-        }
-    }
-    else
-    {
-        *n = 1;
-    }
+    // Handle multi-items.
+    int powerup_set = powerup / (POWERUP_LAST - POWERUP_FIRST + 1);
+    powerup = powerup % (POWERUP_LAST - POWERUP_FIRST + 1);
+    // This structure is used to make it easy to mod in different values.
+    *n = (powerup_set == 0) ? 1 :
+         (powerup_set == 1) ? 2 : 3;
 
     // Prevents some items early on:
     // - Cakes right after the start destroy too much and too easily
@@ -934,7 +923,7 @@ PowerupManager::PowerupType PowerupManager::getRandomPowerup(unsigned int pos,
         }
     }
 
-    // Prevent basketballs from being received when at least a kart has
+    // Prevent basketballs from being obtained when at least a kart has
     // already finished:
     // - Avoids the first remaining kart receiving a basket ball
     // - Avoids having a very small space between the player getting the

src/tracks/model_definition_loader.cpp
@@ -53,7 +53,7 @@ void ModelDefinitionLoader::addModelDefinition(const XMLNode* xml)
     std::string model_name;
     xml->get("model", &model_name);
 
-    m_lod_groups[lodgroup].push_back(ModelDefinition(xml, (int)lod_distance, model_name, false, skeletal_animation));
+    m_lod_groups[lodgroup].push_back(ModelDefinition(xml, (int)lod_distance, model_name, skeletal_animation));
 }   // addModelDefinition
 
 // ----------------------------------------------------------------------------

src/tracks/model_definition_loader.hpp
@@ -43,7 +43,6 @@ namespace irr
 struct ModelDefinition
 {
     std::string m_model_file;
-    bool m_tangent; // obsolete, TODO remove
     const XMLNode* m_xml;
 
     /** For LOD */
@@ -54,16 +53,14 @@ struct ModelDefinition
     /** Constructor to allow storing this in STL containers */
     ModelDefinition()
     {
-        m_tangent = false;
         m_skeletal_animation = false;
         m_distance = 0;
         m_xml = NULL;
     }
 
-    ModelDefinition(const XMLNode* xml, int distance, std::string& model, bool tangent, bool skeletal_animation)
+    ModelDefinition(const XMLNode* xml, int distance, std::string& model, bool skeletal_animation)
     {
         m_model_file = model;
-        m_tangent = tangent;
         m_xml = xml;
         m_distance = distance;
         m_skeletal_animation = skeletal_animation;

commit 7cf33d5813d82951a9d2b0dc28e9dedba24719ef

Fix a significant bug in powerup distribution
The code simplification introduced in 638b92f had a simple pitfall : for the quotient and remainder logic to work adequately, the internal powerup values returned by getRandomItem needed to start at 0, whereas they start at 1.

As a result, the last powerup got interpreted as a POWERUP_NOTHING.

Until now, this bug was masked by the fact that the anvil/anchor had probabilities of 0 so nothing seemed amiss.

- Also remove a remaining mention of the anchor powerup

src/items/powerup_manager.cpp
@@ -107,7 +107,7 @@ PowerupManager::PowerupType
         "",            /* Nothing */
         "bubblegum", "cake", "bowling", "zipper", "plunger", "switch",
         "swatter", "rubber-ball", "parachute", "nitro-hack", "electro-shield",
-        "mini-wish", "anchor"
+        "mini-wish"
     };
 
     for(unsigned int i=POWERUP_FIRST; i<=POWERUP_LAST; i++)
@@ -893,9 +893,17 @@ PowerupManager::PowerupType PowerupManager::getRandomPowerup(unsigned int pos,
                                                              uint64_t random_number)
 {
     int powerup = m_current_item_weights.getRandomItem(pos-1, random_number);
-    // Handle multi-items.
-    int powerup_set = powerup / (POWERUP_LAST - POWERUP_FIRST + 1);
-    powerup = powerup % (POWERUP_LAST - POWERUP_FIRST + 1);
+    // POWERUP_FIRST is > 0 but we need powerups to start at 0 and finish
+    // at N-1 where N is the number of powerups for the maths extracting
+    // the powerup type and set to work correctly.
+    // The quotient of the integer division gives us the powerup set
+    // (important for multi-item handling) and the remainder the powerup type
+    powerup -= POWERUP_FIRST;
+    int powerup_count = POWERUP_LAST - POWERUP_FIRST + 1;
+    int powerup_set = powerup / powerup_count;
+    powerup = powerup % powerup_count;
+    powerup += POWERUP_FIRST; // Undo the earlier substraction
+
     // This structure is used to make it easy to mod in different values.
     *n = (powerup_set == 0) ? 1 :
          (powerup_set == 1) ? 2 : 3;

src/items/powerup_manager.hpp
@@ -149,7 +149,7 @@ public:
                       POWERUP_RUBBERBALL, POWERUP_PARACHUTE,
                       POWERUP_SUDO, POWERUP_ELECTRO,
                       POWERUP_MINI,
-                      POWERUP_LAST=POWERUP_MINI,
+                      POWERUP_LAST = POWERUP_MINI,
                       POWERUP_MAX
     };
 